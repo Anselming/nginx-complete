f8be595c54869ea11cf8fc4e14409aca4ddfb2a0
Thiago Anselme <anselme.thiago@gmail.com>
2022-07-23 09:31:22 -0300

    Criada a feature de dockerfile para C# / CSharp

---
+++ mainline/withcsharp/app/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Olá, sou o Thiago Anselme!!!@!");
--- mainline/withcsharp/app/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void main()
-{
-    char* boasvindas = "Olá Mundo, Thiago Anselme escrito em C/C++ com nome!";    
-    printf("%s\\naaa",boasvindas);
-    return;
-}
