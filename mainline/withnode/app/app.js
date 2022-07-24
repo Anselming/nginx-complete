996b077b784f26b7033800e18167bcaf954bb174
Thiago Anselme <anselme.thiago@gmail.com>
2022-07-24 11:56:17 -0300

    Solução funcional construída em NodeJS

--- mainline/withnode/app/app.js
+++ mainline/withnode/app/app.js
@@ -3,9 +3,13 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Hello Wosdfdfrld!')
+  res.send('Olá Thiago Anselme!')
 })
 
+app.get('/Contato', (req, res) => {
+    res.send('<h1>Contato</h1><p>Meu contato é <a href=\'mailto://thiago@anselme.com.br\'>Thiago Anselme</a></p>')
+  })
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Ouvindo na porta: ${port}`)
 })
