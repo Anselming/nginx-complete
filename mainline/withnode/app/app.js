f7cabd477f9e1c0ce3976867240676c905b92b4c
Thiago Anselme <anselme.thiago@gmail.com>
2022-07-24 11:58:45 -0300

    Revert "Solução funcional construída em NodeJS"
    This reverts commit 996b077b784f26b7033800e18167bcaf954bb174.

--- mainline/withnode/app/app.js
+++ mainline/withnode/app/app.js
@@ -3,13 +3,9 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Olá Thiago Anselme!')
+  res.send('Hello Wosdfdfrld!')
 })
 
-app.get('/Contato', (req, res) => {
-    res.send('<h1>Contato</h1><p>Meu contato é <a href=\'mailto://thiago@anselme.com.br\'>Thiago Anselme</a></p>')
-  })
-
 app.listen(port, () => {
-  console.log(`Ouvindo na porta: ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
