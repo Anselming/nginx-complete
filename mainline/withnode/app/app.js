da3b253143e377d0c3d410f30fe8767cae4afba1
Thiago Anselme <anselme.thiago@gmail.com>
2022-07-24 12:15:27 -0300

    Aplicaç~]a

--- mainline/withnode/app/app.js
+++ mainline/withnode/app/app.js
@@ -3,8 +3,12 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Hello Wosdfdfrld!')
+  res.send('Thiago Anselme!')
 })
+
+app.get('/contato', (req, res) => {
+    res.send('<H1>Contato</H1><P>Meu contato é <a href=\'mailto:thiago@anselme.com.br\'>Thiago Anselme</a></P>')
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
