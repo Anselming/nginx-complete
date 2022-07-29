8bace787437634e32493c225ee9e235b83e4bb4b
Thiago Anselme <anselme.thiago@gmail.com>
2022-07-29 02:23:44 -0300

    Docker-compose adicionado

--- mainline/withcsharp/app/Program.cs
+++ mainline/withcsharp/app/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Olá, sou o Thiago Anselme!!!@!");
+int i =0;
+while(true)
+{
+    Console.WriteLine("teste: {0}",i++);
+    Thread.Sleep(5000);
+}
